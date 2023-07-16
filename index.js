23b2f51ea7422e66a8c99596616e127cc232d2c8
Sara Promadee <apromadee@gmail.com>
2023-07-16 16:12:03 +0100

    upload product page and linked all the pages

--- index.js
+++ index.js
@@ -6,7 +6,20 @@ const incrementValue = () => {
     const basketAmount = document.getElementById("basketAmount");
     basketAmount.style.display = "block"
     basketAmount.innerHTML = count;
-  };
+   
 
+  };
+  
+  let loveCount = 0;
+
+  const loveIncrementValue = () => {
+    loveCount++;
+    const loveItems = document.getElementById("loveItems");
+    loveItems.style.display = "block"
+    loveItems.innerHTML = loveCount;
+
+    console.log(loveCount);
+  }
+  
 
 
